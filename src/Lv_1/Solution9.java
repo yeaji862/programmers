caaeb97ba28b43c1eb724da1f78d3cfbdd14fee8
yeaji862 <kimyj@naver.com>
2023-07-05 19:12:29 +0900

    Lv1 대충 만든 자판

---
+++ src/Lv_1/Solution9.java
@@ -0,0 +1,30 @@
+package Lv_1;
+
+public class Solution9 { // 대충 만든 자판
+        public int[] solution(String[] keymap, String[] targets) {
+            int[] answer = new int[targets.length];
+
+            for(int i=0; i<targets.length; i++){
+
+                for(int j=0; j<targets[i].length(); j++){
+                    int small = 101;
+                    for(int p=0; p<keymap.length; p++){
+                        int ind = keymap[p].indexOf(targets[i].charAt(j))+1;
+                        if(small > ind && ind != 0) {
+                            small = ind;
+                        }
+                    }
+
+                    if(small == 101){
+                        answer[i] = -1;
+                        break;
+                    }else{
+                        answer[i] += small;
+                    }
+                }
+            }
+
+            return answer;
+        }
+
+}
